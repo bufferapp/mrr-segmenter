1042a49aa0a110032400a085b48f64e219d7b3cb
David Gasquez <davidgasquez@gmail.com>
2018-06-06 09:36:08 +0200

    :art:

--- mrr_source.R
+++ mrr_source.R
@@ -4,21 +4,21 @@ library(RPostgres)
 
 # connect to redshift
 redshift_connect <- function() {
-  
+
   if(Sys.getenv("REDSHIFT_USER") == "" | Sys.getenv("REDSHIFT_PASSWORD") == "") {
     user <- readline(prompt="Enter your Redshift user: ")
     pwd <- readline(prompt="Enter your Redshift password: ")
     Sys.setenv(REDSHIFT_USER = user)
     Sys.setenv(REDSHIFT_PASSWORD = pwd)
   }
-  
+
   con <- dbConnect(RPostgres::Postgres(),
                    host=Sys.getenv("REDSHIFT_ENDPOINT"),
                    port=Sys.getenv("REDSHIFT_DB_PORT"),
                    dbname=Sys.getenv("REDSHIFT_DB_NAME"),
                    user=Sys.getenv("REDSHIFT_USER"),
                    password=Sys.getenv("REDSHIFT_PASSWORD"))
-  
+
   con
 }
 
@@ -30,13 +30,13 @@ query_db <- function(query, connection) {
 
 # get data from Redshift
 get_redshift_data <- function() {
-  
+
   # connect to redshift
   con <- redshift_connect()
-  
+
   # define mrr query
   mrr_query <- "
-    select 
+    select
       date(date) as date
       , gateway
       , simplified_plan_id
@@ -47,12 +47,12 @@ get_redshift_data <- function() {
     and date < (current_date - 2)
     group by 1, 2, 3, 4
     "
-  
+
   # query redshift
   mrr_data <- query_db(mrr_query, con)
-  
+
   # fix up billing intervals
-  mrr_data <- mrr_data %>% 
+  mrr_data <- mrr_data %>%
     mutate(billing_interval = ifelse(billing_interval == 'Annual' | billing_interval == 'Yearly', 'year',
                                      ifelse(billing_interval == 'Monthly', 'month',
                                             ifelse(billing_interval == 'Quarterly', 'year', billing_interval))))
@@ -62,10 +62,10 @@ get_redshift_data <- function() {
 
 # function to save data
 save_data <- function(df) {
-  
+
   # create a unique file name
   filename <- sprintf("%s_%s.csv", as.character(Sys.Date()), 'mrr')
-  
+
   # Write the file to the local system
   write.csv(df, file = file.path('.', filename), row.names = FALSE)
 }
@@ -73,24 +73,24 @@ save_data <- function(df) {
 
 # function to read data
 load_data <- function() {
-  
+
   # get filename
   filename <- sprintf("%s_%s.csv", as.character(Sys.Date()), 'mrr')
-  
+
   # read csv
   df <- read.csv(file = file.path('.', filename), stringsAsFactors = FALSE)
-  
+
   df
 }
 
 
 # function to get data
 get_mrr_data <- function() {
-  
+
   # get filename
   filename <- sprintf("%s_%s.csv", as.character(Sys.Date()), 'mrr')
   file = file.path('.', filename)
-  
+
   # check if file exists
   if (file.exists(file)) {
     df <- load_data()
@@ -98,10 +98,10 @@ get_mrr_data <- function() {
     df <- get_redshift_data()
     save_data(df)
   }
-  
-  # set dates 
+
+  # set dates
   df$date <- as.Date(df$date, format = '%Y-%m-%d')
-  
+
   # return data frame
   df
 }
